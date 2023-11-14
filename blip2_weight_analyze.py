4bc1766300564912acaf07940881e9aa4a3216ab
vic <wfeng@g.harvard.edu>
2023-11-14 13:31:42 -0500

    WIP: refactor the code

--- blip2_weight_analyze.py
+++ blip2_weight_analyze.py
@@ -29,16 +29,11 @@ def analyze():
     processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
     # by default `from_pretrained` loads the weights in float32
     model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float32) 
-    # encoder_fc_pattern = re.compile(r'vision_model\.encoder\.layers\.(\d+)\.mlp\.fc1\.weight') 
-    # decoder_fc_pattern = re.compile(r'language_model\.model\.decoder\.layers\.(\d+)\.fc1\.weight') 
-    encoder_atten_pattern = re.compile(r'vision_model\.encoder\.layers\.(\d+)\.self_attn\.projection\.weight') 
-    decoder_atten_pattern = re.compile(r'language_model\.model\.decoder\.layers\.(\d+)\.self_attn\.out_proj\.weight')
-    qformer_atten_pattern = re.compile(r'qformer\.encoder\.layer\.(\d+)\.attention\.output\.dense\.weight') 
-
-    encoder_data = find_parameters_based_on_patterns(model, encoder_atten_pattern)
-    decoder_data = find_parameters_based_on_patterns(model, decoder_atten_pattern)
-    qformer_data =  find_parameters_based_on_patterns(model, qformer_atten_pattern)
+    encoder_data = find_parameters_based_on_patterns(model, Pattern.encoder_atten_pattern)
+    decoder_data = find_parameters_based_on_patterns(model, Pattern.decoder_atten_pattern)
+    qformer_data =  find_parameters_based_on_patterns(model, Pattern.qformer_atten_pattern)
     plot((encoder_data, decoder_data, qformer_data), outname="blip2-opt-2.7b-attention-weight.png")
+    print("Finisn analysis")
 
 if __name__ == "__main__":
     analyze()
--- lib.py
+++ lib.py
@@ -1,7 +1,23 @@
-def find_parameters_based_on_patterns(model, pattern):
+from enum import Enum
+import re
+
+"""
+Patterns to match the parameter
+"""
+class Pattern(Enum):
+    # fc1 weight parameter pattern
+    encoder_fc_pattern = r'vision_model\.encoder\.layers\.(\d+)\.mlp\.fc1\.weight'
+    decoder_fc_pattern = r'language_model\.model\.decoder\.layers\.(\d+)\.fc1\.weight'
+    # W_o weight parameter pattern in attention layer
+    encoder_atten_pattern = r'vision_model\.encoder\.layers\.(\d+)\.self_attn\.projection\.weight' 
+    decoder_atten_pattern = r'language_model\.model\.decoder\.layers\.(\d+)\.self_attn\.out_proj\.weight'
+    qformer_atten_pattern = r'qformer\.encoder\.layer\.(\d+)\.attention\.output\.dense\.weight' 
+
+def find_parameters_based_on_patterns(model, pattern: Pattern):
     """
-    Match model parameter names with pattern
+    Match model parameter names with pattern (regular expression string)
     """
+    pattern = re.compile(pattern.value)
     model_parameters = model.state_dict()
     data = []
     for key in model_parameters.keys():
