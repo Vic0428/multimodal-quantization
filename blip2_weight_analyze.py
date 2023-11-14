a9e17475aac9c757f3d60053eb5f2021eb9de4c7
vic <wfeng@g.harvard.edu>
2023-11-14 13:45:27 -0500

    Finish refactor

--- blip2_weight_analyze.py
+++ blip2_weight_analyze.py
@@ -1,7 +1,5 @@
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
 import torch
-import re
-import matplotlib.pyplot as plt
 from lib import *
 from plot import *
 
@@ -10,11 +8,12 @@ def analyze():
     # by default `from_pretrained` loads the weights in float32
     model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float32) 
     encoder_data = find_parameters_based_on_patterns(model, Pattern.encoder_atten_pattern)
-    decoder_data = find_parameters_based_on_patterns(model, Pattern.decoder_atten_pattern)
-    qformer_data =  find_parameters_based_on_patterns(model, Pattern.qformer_atten_pattern)
-    print(encoder_data)
-    # plot((encoder_data, decoder_data, qformer_data), outname="blip2-opt-2.7b-attention-weight.png")
-    print("Finisn analysis")
+    # The type of encoder data: List[Parameter]
+    for p in encoder_data:
+        print(f"[Layer={p.layer_id}] parameter mean={np.mean(p.data)}")
+    
+    print("Finish analysis")
+
 
 if __name__ == "__main__":
     analyze()
